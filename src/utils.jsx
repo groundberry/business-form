b2b2648aadbb77e74cc6c1f366fea577889409c1
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-09-19 19:00:08 -0700

    Change iframe url to https

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,5 +1,5 @@
 export function getBusinessURL(businessInfo) {
-  const url = 'http://cloudtemplates.cloudfrontend.net/app/live-preview/?clone_id=1576931&site=';
+  const url = 'https://cloudtemplates.cloudfrontend.net/app/live-preview/?clone_id=1576931&site=';
   const siteUrl = JSON.stringify({ business: businessInfo });
   const encodedSiteUrle = encodeURIComponent(siteUrl);
   return url + encodedSiteUrle;
